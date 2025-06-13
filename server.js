f8a741156a6ea4ce02872f99750d2f3d1c21085c
Flavien-Walk <flavien.walkowiak@epitech.digital>
2025-06-13 11:29:47 +0200

    ajout du système de MAJ

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Route pour latest.json
+app.get("/latest.json", (req, res) => {
+  res.sendFile(__dirname + "/latest.json");
+});
+
 // Routes
 app.use("/api/bottles", bottleRoutes);
 
